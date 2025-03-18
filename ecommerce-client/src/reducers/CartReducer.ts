82cdeb84195cba193b36d80a1be9eb5bf3f32b18
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-03-18 09:54:48 +0100

    add item done

---
+++ ecommerce-client/src/reducers/CartReducer.ts
@@ -0,0 +1,44 @@
+import { CartItem } from "../models/CartItem";
+
+export interface ICartAction {
+    type: CartActionType;
+    payload: CartItem;
+}
+
+export enum CartActionType {
+ADD_ITEM,
+REMOVE_ITEM,
+CHANGE_QUANTITY,
+RESET_CART
+}
+
+export const CartReducer = (cart: CartItem[], action: ICartAction) => {
+    const {payload, type} = action;
+
+    switch(type) {
+        case CartActionType.ADD_ITEM: {
+            const itemExists = cart.find((item) => item.product.id === payload.product.id);
+            if(!itemExists) return [...cart, payload];
+
+            return cart.map((item) => (
+                item.product.id === payload.product.id 
+                ? {...item, quantity: item.quantity + payload.quantity}
+                : item
+            ))
+        
+        }
+        case CartActionType.REMOVE_ITEM: {
+            
+        }
+        case CartActionType.CHANGE_QUANTITY:
+        {
+           
+        }
+        case CartActionType.RESET_CART: {
+            
+        }
+        default: {
+            return cart;
+        }
+    }
+}
