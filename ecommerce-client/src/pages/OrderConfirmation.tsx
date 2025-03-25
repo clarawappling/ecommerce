de5e565922484ea22ed5ba3d5ab6e22e6291810d
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-03-25 12:21:58 +0100

    order details presented on confirmation page

--- ecommerce-client/src/pages/OrderConfirmation.tsx
+++ ecommerce-client/src/pages/OrderConfirmation.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router"
 import { useOrder } from "../hooks/useOrder";
+import { DetailedOrder } from "../models/Order";
+import "../styles/OrderConfirmation.css"
 
 export const OrderConfirmation = () => {
     const location = useLocation();
 
-    // Hoppa över statet? Kommer inte hinna sättas i useEffecten. Sätt order-infon sen istället.
+
     const [paymentId, setPaymentId] = useState<string |null>(null);
     const {fetchOrderByPaymentIdHandler} = useOrder();
+    const [order, setOrder] = useState <DetailedOrder | null> (null)
 
     useEffect(() => {
         const queryParams = new URLSearchParams(location.search);
@@ -17,7 +20,8 @@ export const OrderConfirmation = () => {
 
         const getOrderBySessionId = async () => {
             if(paymentIdQuery) {
-                const order =  await fetchOrderByPaymentIdHandler(paymentIdQuery)
+                const data =  await fetchOrderByPaymentIdHandler(paymentIdQuery)
+                setOrder(data)
                 console.log(order)
             }
            
@@ -26,11 +30,29 @@ export const OrderConfirmation = () => {
     }, [location.search])
 
 
-    
+    // Loading spinner
     return (
         <>
+        {/* Villkor order hämtad */}
         <h1>Orderbekräftelse</h1>
-        <p>{paymentId}</p>
+        <p>Tack för din beställning, {order?.customer_firstname}! 
+        Vi är så glada över att du valt att handla hos oss.</p> <p>Nedan hittar du en sammanfattning av den order som är på väg hem till dig.</p>
+        
+            <h2>Orderdetaljer</h2>
+            <div className="order-details-container">
+            {order?.order_items.map((item) => {
+                return (
+                    <div key={item.id} className="order-item">
+                    <h4>{item.product_name}</h4>
+                    <p>Antal: {item.quantity} st</p>
+                    <p>á pris: {item.unit_price} SEK</p>
+                    <p>Totalt pris: {item.quantity * item.unit_price} SEK</p>
+                    </div>
+                )
+            })}
+            </div>
+            <h3>Total ordersumma: {order?.total_price} SEK</h3>
+        
         </>
     )
 }
