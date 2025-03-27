b5bac2a3ca8390b76ecfe44e5d43027e90a72ede
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-03-27 14:53:43 +0100

    footer in place

---
+++ ecommerce-client/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css"
+
+export const Footer = () => {
+    return (
+        <>
+          <p>&copy; 2025 Juguetes Escondidos. All rights reserved.</p>
+    <p>
+        <a href="/">GDPR</a> | 
+        <a href="/">Kontakt</a>
+    </p>
+        </>
+    )
+}
